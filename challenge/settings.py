2567ee0b5f382d6df357069ee628f75e86683499
enzoGiovannetti <enzogtti@gmail.com>
2022-04-16 00:58:30 -0300

    agora eu consegui

--- challenge/settings.py
+++ challenge/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'import_transa',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
